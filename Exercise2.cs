f72a2669f8a76b84b1b104842205fe028cf8cf3b
Jason Mathew <92264526+Lightblaz@users.noreply.github.com>
2022-07-28 16:17:33 +0530

    C# exercise files

---
+++ Exercise2.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1
+            /*Console.WriteLine("Hello World!");*/
+
+
+            //2
+            /*Console.WriteLine("Hello");
+            Console.WriteLine("Programming");*/
+
+            //3
+            /*Console.WriteLine("Enter your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);*/
+
+            //4
+            /*Console.WriteLine("Enter 2 numbes");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The addition is " + (num1+num2));*/
+
+            //5
+            /*Console.WriteLine("Enter the salesman number");
+            int salesnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the salesman Name");
+            String SalesManName = Console.ReadLine();
+            Console.WriteLine("Enter the number of units sold");
+            int unitssold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the unit price");
+            float unitprice = float.Parse(Console.ReadLine());
+            Console.WriteLine("The salesman number - " + salesnumber);
+            Console.WriteLine("The salesman name - " + SalesManName);
+            float salesvalue = unitssold * unitprice;
+            Console.WriteLine("The SalesValue - " + salesvalue);*/
+
+
+            //7
+            /*Console.WriteLine("Enter the student number");
+            int studentnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the student Name");
+            String studentname = Console.ReadLine();
+            Console.WriteLine("Enter the student Makrs");
+            int mark1 = Convert.ToInt32(Console.ReadLine());
+            int mark2 = Convert.ToInt32(Console.ReadLine());
+            int mark3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Student Number - " + studentnum);
+            Console.WriteLine("Student Name - " + studentname);
+            int total = mark1 + mark2 + mark3;
+            Console.WriteLine("Total marks - " + total);
+            float averagemarks = total / 3;
+            Console.WriteLine("Average marks - " + averagemarks);*/
+
+            //8
+            /*if(averagemarks > 5)
+            {
+                Console.WriteLine("pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }*/
+
+            //9
+            /*if(averagemarks >= 80)
+            {
+                Console.WriteLine("Distinction");
+            }else if(averagemarks >= 70)
+            {
+                Console.WriteLine("Very Good pass");
+            }else if(averagemarks >= 60)
+            {
+                Console.WriteLine("Credit");
+            }else if(averagemarks >= 50)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }*/
+
+            //10
+            /*Console.WriteLine("Enter the basic salary");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            double commission = 0;
+            if (salesvalue > 50000)
+            {
+                commission = (salesvalue * 0.1);
+            }
+            if(salary > 25000)
+            {
+                Console.WriteLine("Final salary - " + (salary + commission));
+                Console.WriteLine("Commission - " + commission);
+            }*/
+
+            //11 circle
+            //const float pi = 3.142f;
+            /*Console.WriteLine("Enter the raduis to calculate the circumference and area");
+            float raduis = float.Parse(Console.ReadLine());
+            Console.WriteLine("Circumference - " + (2 * pi * raduis ));
+            Console.WriteLine("Area - " + (pi * raduis));*/
+
+            //12 rectangle
+            /*Console.Write("Enter the Height : ");
+            float Height = float.Parse(Console.ReadLine());
+            Console.Write("Enter the Width : ");
+            float Width = float.Parse(Console.ReadLine());
+            Console.WriteLine("Perimeter - " + 2 *(Height + Width));
+            Console.WriteLine("Area - " + (Height * Width));*/
+
+            //13 cylinder
+            /*Console.Write("Enter the Raduis : ");
+            float craduis = float.Parse(Console.ReadLine());
+            Console.Write("Enter the Height : ");
+            float Cheight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Surface Area - " + (2 * pi * craduis + Cheight));
+            Console.WriteLine("Volume - " + (pi * (craduis * craduis) * Cheight));*/
+
+            
+            Console.WriteLine("Enter your choice");
+            Console.WriteLine("1: The area and the circumference of the circle");
+            Console.WriteLine("2: The perimeter and the area of the rectangle");
+            Console.WriteLine("3: The surface area and the volume of the cylinder");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    circle();
+                    break;
+                case 2:
+                    rectangle();
+                    break;
+                case 3:
+                    cylinder();
+                    break;
+                default:
+                    Console.WriteLine("Enter a number between 1 and 3");
+                    break;
+            }
+        }
+
+        static void circle()
+        {
+            const float pi = 3.142f;
+            Console.WriteLine("Enter the raduis to calculate the circumference and area");
+            float raduis = float.Parse(Console.ReadLine());
+            Console.WriteLine("Circumference - " + (2 * pi * raduis ));
+            Console.WriteLine("Area - " + (pi * raduis));
+        }
+        static void rectangle()
+        {
+            Console.Write("Enter the Height : ");
+            float Height = float.Parse(Console.ReadLine());
+            Console.Write("Enter the Width : ");
+            float Width = float.Parse(Console.ReadLine());
+            Console.WriteLine("Perimeter - " + 2 *(Height + Width));
+            Console.WriteLine("Area - " + (Height * Width));
+        }
+        static void cylinder()
+        {
+            const float pi = 3.142f;
+            Console.Write("Enter the Raduis : ");
+            float craduis = float.Parse(Console.ReadLine());
+            Console.Write("Enter the Height : ");
+            float Cheight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Surface Area - " + (2 * pi * craduis + Cheight));
+            Console.WriteLine("Volume - " + (pi * (craduis * craduis) * Cheight));
+        }
+    }
+}
---
+++ Exercise3.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            displayoptions();
+        }
+
+        static void displayoptions()
+        {
+            //1
+            /*Console.WriteLine("1: +");
+            Console.WriteLine("2: -");
+            Console.WriteLine("3: *");
+            Console.WriteLine("4: /");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the numbers");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            float total = 0f;
+            switch (choice)
+            {
+                case 1:
+                    total = add(num1, num2);
+                    break;
+                case 2:
+                    total = minus(num1, num2);
+                    break;
+                case 3:
+                    total = mul(num1, num2);
+                    break;
+                case 4:
+                    total = divide(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Enter number between 1 and 4");
+                    break;
+            }
+
+            Console.WriteLine("The total is " + total);*/
+
+            //2
+            /*int total = 0;
+            for(int i =0; i < 11; i++)
+            {
+                total += i;
+            }
+            Console.WriteLine("The total is " + total);*/
+
+            //3
+            /*for(int i =0; i < 101; i++)
+            {
+                if(i%2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }*/
+
+            //4
+            /*Console.WriteLine("Enter a range");
+            int start = Convert.ToInt32(Console.ReadLine());
+            int end = Convert.ToInt32(Console.ReadLine());
+            int total = 0;
+            for(int i = start; i < end; i++)
+            {
+                if( i%2 == 0)
+                {
+                    total += i;
+                }
+            }
+            Console.WriteLine("The total is " + total);*/
+
+
+            //5
+            Console.WriteLine("Enter a Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(num +" x " + i + " = " + (num*i));
+            }
+        }
+        static float add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+        static float minus(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+        static float mul(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+        static float divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
