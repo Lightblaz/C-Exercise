81dbec9519e210a7bebaf8fa4fb14a02ef1222df
Jason Mathew <92264526+Lightblaz@users.noreply.github.com>
2022-07-28 16:53:07 +0530

    updated solutiion

--- Exercise3.cs
+++ Exercise3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise3
 {
@@ -75,13 +76,131 @@ namespace Exercise3
 
 
             //5
-            Console.WriteLine("Enter a Number");
+            /*Console.WriteLine("Enter a Number");
             int num = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < 11; i++)
             {
                 Console.WriteLine(num +" x " + i + " = " + (num*i));
+            }*/
+
+            //6
+            /*int total = 0, num , count=0;
+            float average = 0f;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            while(num >= 0)
+            {
+                total += num;
+                count++;
+                Console.WriteLine("Enter a number");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The total is " + total);
+            Console.WriteLine("The average is " + (total/count));*/
+
+
+            //7
+            /*int pustotal = 0 , negtotal = 0, num;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                if(num > 0)
+                {
+                    pustotal += num;
+                }
+                else
+                {
+                    negtotal += num;
+                }
+                Console.WriteLine("Enter a number");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The pos total is " + pustotal);
+            Console.WriteLine("The neg total is " + negtotal);*/
+
+            //8
+            /*int[] array1 = new int[10];
+            float total = 0;
+            int num;
+            for(int i =0; i < 10; i++)
+            {
+                Console.WriteLine("Enter a number");
+                num = Convert.ToInt32(Console.ReadLine());
+                array1[i] = num;
+                total += array1[i];
+            }
+
+            Console.WriteLine("Total is " + total);
+            Console.WriteLine("Average is " + (total / array1.Length));*/
+
+            //9
+            /*int[] array1 = new int[10];
+            float total = 0;
+            int num;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter a number");
+                num = Convert.ToInt32(Console.ReadLine());
+                array1[i] = num;
+                total += array1[i];
+            }
+            int n = array1.Length , val , flag;
+            for (int i = 1; i < n; i++)
+            {
+                val = array1[i];
+                flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < array1[j])
+                    {
+                        array1[j + 1] = array1[j];
+                        j--;
+                        array1[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+
+            Console.WriteLine("\nSorted Array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array1[i] + " ");
+            }*/
+
+            //10
+            /*for(int i =0; i < 5; i++)
+            {
+                for(int j=0; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }*/
+
+            //11
+            /*for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < (i+1); j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }*/
+
+            //12
+            int[] array1 = { 1, 4, 2, 86, 7 };
+            int[] array2 = { 7, 5, 9, 14, 0, 2, 6 }; 
+            var intersect = array1.Intersect(array2);
+
+            foreach (int value in intersect)
+            {
+                Console.WriteLine(value);
             }
         }
+
         static float add(float num1, float num2)
         {
             return num1 + num2;
